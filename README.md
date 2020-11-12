66f03a439fab3d8f58336042dfec27f163840d8b
Mohamad Safadieh <me@msafadieh.com>
2020-11-12 03:29:52 -0500

    preliminary IPv6 support and handle bad SSL

--- README.md
+++ README.md
@@ -16,8 +16,8 @@ Do not use in production. I only wrote this to learn more about TCP/IP and Pytho
 An SSL certificate and private key are mandatory per the Gemini specifications. By default, the server will bind to all available interfaces on **port 1965**. The default queue size is 32 and the default thread count is 4.
 
 ```
-usage: gemgem.py [-h] [-b HOST] [-p PORT] [-c CERT] [-k KEY] [-w WEBROOT]
-                 [-q QUEUE] [-t THREADS]
+usage: gemgem.py [-h] [-b HOST] [-p PORT] [-6] [-b6 HOST_V6] [-p6 PORT_V6]
+                 [-c CERT] [-k KEY] [-w WEBROOT] [-q QUEUE] [-t THREADS]
 
 A multi-threaded gemini server
 
@@ -25,6 +25,11 @@ optional arguments:
   -h, --help            show this help message and exit
   -b HOST, --host HOST  Host to bind to
   -p PORT, --port PORT  Port to bind to
+  -6, --ipv6            Enable IPv6
+  -b6 HOST_V6, --host-v6 HOST_V6
+                        IPv6 host to bind to
+  -p6 PORT_V6, --port-v6 PORT_V6
+                        IPv6 port to bind to
   -c CERT, --cert CERT  SSL certificate in PEM format
   -k KEY, --key KEY     SSL private key in PEM format
   -w WEBROOT, --webroot WEBROOT
--- gemgem.py
+++ gemgem.py
@@ -21,6 +21,7 @@ DEFAULT_THREADS = 4
 
 DEFAULT_HOST = ""
 DEFAULT_PORT = 1965
+DEFAULT_HOST_V6 = "::1"
 DEFAULT_CERTFILE = "cert.pem"
 DEFAULT_KEYFILE = "key.pem"
 DEFAULT_WEBROOT = "."
@@ -38,14 +39,16 @@ def create_context(certfile, keyfile):
     return context
 
 
-def create_socket(host, port):
+def create_socket(host, port, ipv6=False):
     """
     Creates TCP socket and starts listening
 
     host: host to bind to
     port: port to bind to
+    ipv6: whether to create IPv6 socket or not
     """
-    gemsocket = socket.socket()
+    family = socket.AF_INET6 if ipv6 else socket.AF_INET
+    gemsocket = socket.socket(family=family)
     gemsocket.bind(
         (
             host,
@@ -154,11 +157,8 @@ def thread_loop(queue, webroot):
         if stream is None:
             break
 
-        try:
+        with stream:
             handle_request(stream, webroot)
-        finally:
-            stream.shutdown(socket.SHUT_RDWR)
-            stream.close()
 
 
 def start_threads(count, queue, webroot):
@@ -200,10 +200,13 @@ def server_loop(gemsocket, ssl_context, queue):
     """
     while True:
 
-        connsocket, _ = gemsocket.accept()
-        stream = ssl_context.wrap_socket(connsocket, server_side=True)
+        try:
+            connsocket, _ = gemsocket.accept()
+            stream = ssl_context.wrap_socket(connsocket, server_side=True)
 
-        queue.put(stream)
+            queue.put(stream)
+        except OSError:
+            break
 
 
 def parse_args():
@@ -213,6 +216,9 @@ def parse_args():
     parser = ArgumentParser(description="A multi-threaded gemini server")
     parser.add_argument("-b", "--host", default=DEFAULT_HOST, help="Host to bind to")
     parser.add_argument("-p", "--port", default=DEFAULT_PORT, help="Port to bind to")
+    parser.add_argument("-6", "--ipv6", action="store_true", help="Enable IPv6")
+    parser.add_argument("-b6", "--host-v6", default=DEFAULT_HOST_V6, help="IPv6 host to bind to")
+    parser.add_argument("-p6", "--port-v6", default=DEFAULT_PORT, help="IPv6 port to bind to")
     parser.add_argument(
         "-c", "--cert", default=DEFAULT_CERTFILE, help="SSL certificate in PEM format"
     )
@@ -235,13 +241,17 @@ def main():
     """
     Entrypoint function.
     """
+    gemsocketv6 = None
 
     args = parse_args()
     queue = Queue(maxsize=args.queue)
     context = create_context(args.cert, args.key)
     try:
-        gemsocket = create_socket(args.host, args.port)
         start_threads(args.threads, queue, args.webroot)
+        gemsocket = create_socket(args.host, args.port)
+        if args.ipv6:
+            gemsocketv6 = create_socket(args.host_v6, args.port_v6, True)
+            Thread(target=server_loop, args=(gemsocketv6, context, queue, )).start()
         server_loop(gemsocket, context, queue)
     except (KeyboardInterrupt, SystemExit):
         print("Received SIGINT. Shutting down...")
@@ -249,6 +259,9 @@ def main():
         stop_threads(args.threads, queue)
         gemsocket.shutdown(socket.SHUT_RDWR)
         gemsocket.close()
+        if args.ipv6 and gemsocketv6:
+            gemsocketv6.shutdown(socket.SHUT_RDWR)
+            gemsocketv6.close()
 
 
 if __name__ == "__main__":
